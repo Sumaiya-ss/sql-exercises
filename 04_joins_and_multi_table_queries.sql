f864da1f6263640c5130f7d86a5c11834f95b010
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-07-31 16:43:58 +0100

    Update and rename 04_Multi-table queries with JOINs.sql to 04_joins_and_multi_table_queries.sql

---
+++ 04_joins_and_multi_table_queries.sql
@@ -0,0 +1,17 @@
+--Find the domestic and international sales for each movie
+SELECT movies.title, boxoffice.domestic_sales, boxoffice.international_sales
+FROM movies
+INNER JOIN Boxoffice 
+    ON movies.id = boxoffice.movie_id;
+
+-- Show the sales numbers for each movie that did better internationally rather than domestically
+SELECT movies.title, boxoffice.domestic_sales, boxoffice.international_sales
+FROM movies
+INNER JOIN Boxoffice 
+    ON movies.id = boxoffice.movie_id
+    where international_sales > Domestic_Sales;
+
+-- List all the movies by their ratings in descending order
+SELECT title, rating
+FROM movies
+ORDER BY rating DESC;
