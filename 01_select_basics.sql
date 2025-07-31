db3b680c5af6832443f6ec27fc223be421d0d87c
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-07-31 15:45:56 +0100

    Create 01_select_basics.sql

---
+++ 01_select_basics.sql
@@ -0,0 +1,8 @@
+-- Select all columns from the movies table
+SELECT * FROM movies;
+
+-- Select only name from the movies table
+SELECT title FROM movies;
+
+-- Select name and year columns
+SELECT title, year FROM movies;
