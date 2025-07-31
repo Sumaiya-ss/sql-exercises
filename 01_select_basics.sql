caf101685b495f8a22038f9b24cdc154b70d0a82
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-07-31 15:55:22 +0100

    Update 01_select_basics.sql

--- 01_select_basics.sql
+++ 01_select_basics.sql
@@ -6,3 +6,21 @@ SELECT title FROM movies;
 
 -- Select name and year columns
 SELECT title, year FROM movies;
+
+-- Find the movie with a row id of 6
+SELECT * FROM movies
+WHERE id = 6;
+
+-- Find the movies released in the years between 2000 and 2010
+SELECT * FROM movies
+WHERE year BETWEEN 2000 AND 2010;
+
+-- Find the movies NOT released in the years between 2000 and 2010
+SELECT * FROM movies
+WHERE year NOT BETWEEN 2000 AND 2010;
+
+-- Find the first 5 Pixar movies and their release year
+SELECT title, year FROM movies
+ORDER BY year ASC
+LIMIT 5;
+
