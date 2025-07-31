6506658184e7c2a9c9a8c81d585a0417d8493e0e
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-07-31 16:48:39 +0100

    Update 03_Filtering and sorting Query results.sql

--- 03_Filtering and sorting Query results.sql
+++ 03_Filtering and sorting Query results.sql
@@ -18,3 +18,12 @@ SELECT title
 FROM movies
 ORDER BY title ASC
 LIMIT 5 OFFSET 5;
+
+-- Movies released after 2000 and with a rating above 8.5
+SELECT * FROM movies WHERE year > 2000 AND rating > 8.5;
+
+-- Movies released in 1990 or 2000
+SELECT * FROM movies WHERE year = 1990 OR year = 2000;
+
+-- Movies not rated 5
+SELECT * FROM movies WHERE rating != 5;
