f9cf3ac5f186de58ead85a45c49ed8a7ce0b4e18
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-08-06 13:39:16 +0100

    Update 09_calculations_and_functions.sql

--- 09_calculations_and_functions.sql
+++ 09_calculations_and_functions.sql
@@ -1,3 +1,4 @@
+Movie_Queries
 --List all movies and their combined sales in millions of dollars
 SELECT title, ((domestic_sales + international_sales)/ 1000000) 
   FROM movies m
@@ -14,3 +15,19 @@ where m.id = b.movie_id;
 SELECT title, year
 FROM movies
 where year % 2 = 0;
+
+
+Employees_Queries
+--Find the longest time that an employee has been at the studio
+SELECT name,max(years_employed) 
+FROM employees;
+
+--For each role, find the average number of years employed by employees in that role
+SELECT role, avg(years_employed) 
+FROM employees
+group by role;
+
+--Find the total number of employee years worked in each building
+SELECT building, sum(years_employed)
+FROM employees
+group by building;
