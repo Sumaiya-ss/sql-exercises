78f5f022f39eb3b1cf8f94d57f56c9c09e0734f6
Sumaiya-ss <serajsumaiya79@gmail.com>
2025-08-07 15:37:28 +0100

    Update 10_calculations_and_functions_p2.sql

--- 10_calculations_and_functions_p2.sql
+++ 10_calculations_and_functions_p2.sql
@@ -1,3 +1,4 @@
+Employee_queries
 --Find the number of Artists in the studio (without a HAVING clause) 
 SELECT count(role) 
 FROM employees
@@ -12,3 +13,17 @@ group by role;
 SELECT sum(years_employed) 
 FROM employees
 where role = "Engineer";
+
+Movie_ queries
+--Find the number of movies each director has directed 
+SELECT director, count(title) as total_movies FROM movies
+group by director;
+
+--Find the total domestic and international sales that can be attributed to each director
+SELECT director, sum(Domestic_sales + international_sales) As total_sales  
+FROM Movies m
+ join boxoffice b
+ where m.id = b.movie_id
+ group by director;
+
+
